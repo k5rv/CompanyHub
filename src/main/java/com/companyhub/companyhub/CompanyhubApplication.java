2debb0c283d772e896eca585c3d650a5cd09ebdc
k5rv <k.saraev@gmail.com>
2024-04-05 17:10:11 +0300

    Add project

---
+++ src/main/java/com/companyhub/companyhub/CompanyhubApplication.java
@@ -0,0 +1,13 @@
+package com.companyhub.companyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/companyhub/companyhub/CompanyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companyhub.companyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
